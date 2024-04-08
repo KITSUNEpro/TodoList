f4d85fb456b372508b12082d536eb6d62da8a6e7
KITSUNEpro <basuminton829@icloud.com>
2024-04-08 11:57:38 +0900

    FastAPI追加

--- todo-front/src/App.js
+++ todo-front/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = React.useState();
+	const url = "http://127.0.0.1:8000";
+  //fetch()関数を使用してGETリクエストを送信し、その後のPromiseチェーンでレスポンスの処理を行う。
+  const GetData = () => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        //エラーが発生した場合
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+		</div>
+	);
 }
 
 export default App;
